ad11f98df42fc466b0206dc9ea0fbaeafc4e6204
SwapnilR <45485546+SRakhade@users.noreply.github.com>
2024-09-24 20:38:14 +0530

    Update Modify_Plan.py

--- Modify_Plan.py
+++ Modify_Plan.py
@@ -12,8 +12,7 @@ requests.packages.urllib3.disable_warnings()
 usrname = ""
 passwd = ""
 # URL for  BigFix Environment
-#bigfixurl = "https://<RootServername>:52311/api"
-bigfixsaurl = "https://<RootServername>:8443/serverautomation"
+bigfixsaurl = "https://bfrootserver:8443/serverautomation"
 
 # Function to get the automation plan XML 
 def getplan(planid):
