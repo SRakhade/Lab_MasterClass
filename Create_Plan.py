a91a3d8ecc9008c2464657377a222819d32bf127
SwapnilR <45485546+SRakhade@users.noreply.github.com>
2024-09-24 20:36:41 +0530

    Create Create_Plan.py

---
+++ Create_Plan.py
@@ -0,0 +1,46 @@
+import requests
+
+usrname = ""
+passwd = ""
+# URL for SandBox BigFix Environment
+bigfixsaurl = "https://bfrootserver:8443/serverautomation"
+
+def create_plan(xml_input):
+    query = '/plan/master/'
+    r = requests.post(bigfixsaurl + query, auth=(usrname, passwd), data=xml_input, verify=False)
+    return r.text
+    print(r.text)
+
+if __name__ == "__main__":
+    xml_input = '''<?xml version="1.0" encoding="UTF-8"?>
+    <sa-rest xmlns="http://iemfsa.tivoli.ibm.com/REST" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <plan name="CreatePlan" domain="ALL CONTENT" pending-restart="PausePlan" version="2.0" source-plan-id="undefined" prefetch="false" source-plan-site-url="undefined">
+        <description>TEST</description>
+        <category></category>
+        <source></source>
+        <source-severity></source-severity>
+        <source-release-date>2024-09-24</source-release-date>
+        <plan-steps>
+            <step id="101">
+                <fixlet fixlet-id="126" site-type="external" site-name="Server Automation"></fixlet>
+                <!-- <targets><target-group name="myGroup" id="123"/><target-computer name="nameOnlyTarget"/><target-computer name="nameAndIDTarget" id="123"/></targets> -->
+            </step>
+        </plan-steps>
+        <execution-order>
+            <step id="101" depends="">
+                <on-failure action="StopPlan" targets="IncludeFailed" threshold="0"></on-failure>
+            </step>
+        </execution-order>
+        <plan-settings>
+            <boolean-setting name="exclude-non-reporting-endpoint">
+                <![CDATA[true]]>
+            </boolean-setting>
+        </plan-settings>
+    </plan>
+    </sa-rest>'''
+
+  # Function to create a Plan 
+    planid = create_plan(xml_input)
+  # Print the Plan id post creation
+    print(planid)
+    
